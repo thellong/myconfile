bb227bfdf54efbab6ea493c6a252f90781bd31da
thellog <firaxaxa@gmail.com>
2022-07-07 01:02:03 +0700

    nvim colorscheme as catppuccin, some general settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,18 @@
 -- Ola
+--[[
+
+  __  .__           .__  .__                 
+_/  |_|  |__   ____ |  | |  |   ____   ____  
+\   __\  |  \_/ __ \|  | |  |  /  _ \ / ___\ 
+ |  | |   Y  \  ___/|  |_|  |_(  <_> ) /_/  >
+ |__| |___|  /\___  >____/____/\____/\___  / 
+           \/     \/                /_____/  
+
+--]]
+
+require('plugins')
+require('general')
+
+-- Load colorsheme
+vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+vim.cmd[[colorscheme catppuccin]]
